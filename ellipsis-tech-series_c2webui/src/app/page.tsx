8dd60e417c7090013ca44ad1610856dc47da8fa2
Qingyu255 <qingyu2555@gmail.com>
2024-08-30 14:54:38 +0800

    webUI first commit

---
+++ ellipsis-tech-series_c2webui/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-[100vh]">
+      <div>
+        Hello C2 WebUI
+      </div>
+    </main>
+  );
+}
