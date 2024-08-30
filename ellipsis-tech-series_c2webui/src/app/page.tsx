cf5a83ad5d73a84149e7513e3c272bf328be9bfa
Qingyu255 <qingyu2555@gmail.com>
2024-08-31 03:21:20 +0800

    added map

--- ellipsis-tech-series_c2webui/src/app/page.tsx
+++ ellipsis-tech-series_c2webui/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import AlertPopUp from "@/components/AlertPopUp";
 import UserAvatar from "@/components/UserAvatar";
+import MapWithMarker from "@/components/MapWithMarker";
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
         <span>Dashboard</span>
         <UserAvatar username="Keith Tay"/>
       </div>
+      <div className="m-5 h-1/2 md:w-1/3 border-[4px] border-[#c6c8ce] rounded-md">
+        <MapWithMarker lat={1.2976188} long={103.8487032}/>
+      </div>
+      
       
       {/* <AlertPopUp
         itemCategory="weapon"
---
+++ ellipsis-tech-series_c2webui/src/components/MapWithMarker.tsx
@@ -0,0 +1,38 @@
+"use client"
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '100%',
+    height: '50vh'
+};
+
+interface location {
+    lat: number
+    long: number
+};
+
+export default function MapWithMarker({ lat, long }: location) {
+    const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+    if (!GOOGLE_MAPS_API_KEY) {
+        throw new Error("Google Maps API key is missing");
+    }
+
+    const center = {
+        lat: lat || 0,
+        lng: long || 0
+    }
+    
+    return (
+        <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={19}
+            >
+            <Marker position={center} />
+            </GoogleMap>
+        </LoadScript>
+    )
+}
