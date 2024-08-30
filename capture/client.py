84d1a72fc72e6d885995a9fae5138867250dbcce
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-08-30 19:45:26 +0800

    capture

---
+++ capture/client.py
@@ -0,0 +1,144 @@
+import cv2
+import socket
+import pickle
+import struct
+from ultralytics import YOLO
+import math
+
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(("0.0.0.0", 9999))
+
+classNames = [
+    "person",
+    "bicycle",
+    "car",
+    "motorbike",
+    "aeroplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "sofa",
+    "pottedplant",
+    "bed",
+    "diningtable",
+    "toilet",
+    "tvmonitor",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
+
+received_data = b""
+payload_size = struct.calcsize("L")
+
+while True:
+    while len(received_data) < payload_size:
+        received_data = client_socket.recv(4096)
+
+    packed_msg_size = received_data[:payload_size]
+    received_data = received_data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
+
+    while len(received_data) < msg_size:
+        received_data += client_socket.recv(4096)
+
+    frame_data = received_data[:msg_size]
+    received_data = received_data[msg_size:]
+
+    received_frame = pickle.loads(frame_data)
+    results = model(received_frame, stream=True)
+    for r in results:
+        for box in r.boxes:
+            x1, y1, x2, y2 = [int(i) for i in box.xyxy[0]]
+            cv2.rectangle(received_frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            print("Confidence: ", confidence)
+            print("Class name: ", classNames[int(box.cls[0])])
+
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+            cv2.putText(
+                received_frame,
+                classNames[int(box.cls[0])],
+                org,
+                font,
+                fontScale,
+                color,
+                thickness,
+            )
+
+    cv2.imshow("Client Video", received_frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+client_socket.close()
---
+++ capture/server.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import socket
+import pickle
+import struct
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("0.0.0.0", 9999))
+server_socket.listen(10)
+
+client_socket, client_address = server_socket.accept()
+print(f"Accepted connection from {client_address}")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    serialized_frame = pickle.dumps(frame)
+    message_size = struct.pack("L", len(serialized_frame))
+    client_socket.sendall(message_size + serialized_frame)
+
+    cv.imshow("frame", frame)
+    if cv.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
