3a4bb85336b593a8340a1a3c684c862047049f10
Qingyu255 <qingyu2555@gmail.com>
2024-08-31 11:23:24 +0800

    cleanup webhook route

--- ellipsis-tech-series_c2webui/src/app/api/alertingWebhook/route.ts
+++ ellipsis-tech-series_c2webui/src/app/api/alertingWebhook/route.ts
@@ -1,4 +1,3 @@
-import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -9,7 +8,7 @@ export async function POST(request: Request) {
 		const payload = await request.json();
 		const body = JSON.stringify(payload);
 		console.log(body)
-		return NextResponse.json({status: 200});
+		return NextResponse.json({"message":"alert received successfully by C2 WebUI"}, {status: 200});
 	} catch (error) {
 		console.warn(error)
 		return NextResponse.json({ error }, { status: 500 });
