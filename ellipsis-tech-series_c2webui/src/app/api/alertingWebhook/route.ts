a1a64606404fdd5f1b3df20ff286cd55371d89fd
Qingyu255 <qingyu2555@gmail.com>
2024-08-31 11:09:57 +0800

    setup webhook

--- ellipsis-tech-series_c2webui/src/app/api/alertingWebhook/route.ts
+++ ellipsis-tech-series_c2webui/src/app/api/alertingWebhook/route.ts
@@ -4,21 +4,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
 	const headerPayload = headers();
-	// const svix_id = headerPayload.get("svix-id");
-	// const svix_timestamp = headerPayload.get("svix-timestamp");
-	// const svix_signature = headerPayload.get("svix-signature");
-
-	// if (!svix_id || !svix_timestamp || !svix_signature) {
-	// 	return new Response("No svix headers found", {
-	// 		status: 400
-	// 	});
-	// }
 
 	try {
 		const payload = await request.json();
 		const body = JSON.stringify(payload);
-
+		console.log(body)
+		return NextResponse.json({status: 200});
 	} catch (error) {
+		console.warn(error)
 		return NextResponse.json({ error }, { status: 500 });
 	}
 }
